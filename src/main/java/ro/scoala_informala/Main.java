7cf977c9657f1f3c1dbb470641b9b28064c8b6a3
TeodoraG07 <teodora_paraschiv@yahoo.com>
2026-01-07 18:45:35 +0200

    Commit 5

--- src/main/java/ro/scoala_informala/Main.java
+++ src/main/java/ro/scoala_informala/Main.java
@@ -1,17 +1,58 @@
 package ro.scoala_informala;
 
+import java.util.logging.Logger;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    private static final Logger logger =
+            Logger.getLogger(Main.class.getName());
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void main(String[] args) {
+        StudentRepository repository = new StudentRepository();
+        try {
+            Student student = new Student();
+            student.firstName = "Ion";
+            student.lastName = "Bejan";
+            student.gender = "m";
+            student.idCNP = "2980505123470";
+            student.dateOfBirth = 2001;
+            repository.addStudent(student);
+
+
+            Student student2 = new Student();
+            student2.firstName = "Ion";
+            student2.lastName = "Colesov";
+            student2.gender = "m";
+            student2.idCNP = "2980505123460";
+            student2.dateOfBirth = 2001;
+            repository.addStudent(student2);
+
+            System.out.println("Student added successfully!");
+
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error adding student: " + e.getMessage());
         }
+
+//        try {
+//            repository.deleteStudent("2980505123450");
+//            System.out.println("Student deleted successfully");
+//        } catch (IllegalArgumentException e) {
+//            System.out.println("Delete error: " + e.getMessage());
+//        }
+        try {
+            System.out.println("Students aged 25:");
+            repository.retrieveStudentsByAge("25")
+                    .forEach(s -> System.out.println(s.firstName + " " + s.lastName));
+
+            System.out.println("Students ordered by last name:");
+            repository.listStudentsOrderedBy("lastName")
+                    .forEach(s -> System.out.println(s.lastName));
+
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
     }
+
 }
---
+++ src/main/java/ro/scoala_informala/Student.java
@@ -0,0 +1,21 @@
+package ro.scoala_informala;
+
+public class Student {
+    String firstName;
+    String lastName;
+    int dateOfBirth;
+    String gender;
+    String idCNP;
+
+    Student() {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.gender = gender;
+        this.idCNP = idCNP;
+    }
+
+
+}
+
+
